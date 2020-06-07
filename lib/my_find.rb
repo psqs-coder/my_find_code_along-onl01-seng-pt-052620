6da07837a8bfb54980e375dbfecd1bcfd8c0c977
Bruce Parker <detailsharks@gmail.com>
2020-06-07 22:38:41 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,7 +3,9 @@ require 'pry'
 def my_find(collection)
   i = 0 
   while i < collection.length
-  yeild(collection[i])
+  if yield(collection[i])
+    return collection[i]
+  end
   i = i + l
 end
 end
